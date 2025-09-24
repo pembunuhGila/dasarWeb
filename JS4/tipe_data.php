f2c75325cb9f2c19623eddcfe82319fb9daca3e1
= <kiminsetyawan@gmail.com>
2025-09-24 13:13:59 +0700

    Praktikum 2 langkah 5

--- JS4/tipe_data.php
+++ JS4/tipe_data.php
@@ -12,4 +12,17 @@ echo "Variabel d: {$d} <br>";
 echo "Variabel e: {$e} <br>";
 
 var_dump($e);
+
+$nilaiMatematika = 5.1;
+$nilaiIpa = 6.7;
+$nilaiBahasaIndonesia = 9.3;
+
+$rataRata = ($nilaiMatematika + $nilaiIpa + $nilaiBahasaIndonesia) / 3;
+
+echo "Matematika: {$nilaiMatematika} <br>";
+echo "IPA: {$nilaiIpa} <br>";
+echo "Bahasa Indonesia: {$nilaiBahasaIndonesia} <br>";
+echo "Rata-rata: {$rataRata} <br>";
+
+var_dump($rataRata);
 ?>
