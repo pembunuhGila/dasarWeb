5fa9c4fac50da2e1d6b066f61cfdec9d469373fa
= <kiminsetyawan@gmail.com>
2025-09-24 13:47:11 +0700

    Soal 3.1

--- JS4/operator.php
+++ JS4/operator.php
@@ -8,3 +8,10 @@ $hasilKali = $a * $b;
 $hasilBagi = $a / $b;
 $sisaBagi = $a % $b;
 $pangkat = $a ** $b;
+
+echo "Hasil penjumlahan $a + $b = $hasilTambah <br>";
+echo "Hasil pengurangan $a - $b = $hasilKurang <br>";
+echo "Hasil perkalian $a * $b = $hasilKali <br>";
+echo "Hasil pembagian $a / $b = $hasilBagi <br>";
+echo "Sisa hasil bagi $a % $b = $sisaBagi <br>";
+echo "$a pangkat $b = $pangkat <br>";
