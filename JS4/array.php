3b952f8482c31d86eb27f2beb76ecd6e93a0123d
= <kiminsetyawan@gmail.com>
2025-09-25 00:20:04 +0700

    soal 5.4

--- JS4/array.php
+++ JS4/array.php
@@ -60,3 +60,28 @@ foreach ($daftarNilai[$mataKuliah] as $nilai) {
 }
 ?>
 
+<?php
+$daftarNilai = [
+    ['Alice', 85],
+    ['Bob', 92],
+    ['Charlie', 78],
+    ['David', 64],
+    ['Eva', 90],
+];
+
+$total = 0;
+foreach ($daftarNilai as $nilai) {
+    $total += $nilai[1];
+}
+
+$rataRata = $total / count($daftarNilai);
+
+echo "Rata-rata kelas: $rataRata <br>";
+echo "Daftar siswa dengan nilai di atas rata-rata: <br>";
+
+foreach ($daftarNilai as $nilai) {
+    if ($nilai[1] > $rataRata) {
+        echo "Nama: {$nilai[0]}, Nilai: {$nilai[1]} <br>";
+    }
+}
+?>
