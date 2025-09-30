6523fb590842f5c32a3b80be3bae5015b145e8c1
= <kiminsetyawan@gmail.com>
2025-09-30 10:22:33 +0700

    Soal no. 2

--- JS05-PHP02/array_2.php
+++ JS05-PHP02/array_2.php
@@ -4,7 +4,20 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title></title>
-        
+        <style>
+            table {
+                border-collapse: collapse;
+                width: 300px;
+            }
+            th, td {
+                border: 1px solid black;
+                padding: 8px;
+                text-align: left;
+            }
+            th {
+                background-color: #f2f2f2;
+            }
+        </style>
     </head>
     <body>
         <?php
@@ -12,10 +25,24 @@
             'nama' => 'Elok Nur Hamdana',
             'domisili' => 'Malang',
             'jenis_kelamin' => 'Perempuan'];
-
-        echo "Nama : {$dosen['nama']} <br>";
-        echo "Domisili : {$dosen['domisili']} <br>";
-        echo "Jenis Kelamin : {$dosen['jenis_kelamin']} <br>";
         ?>
+        <table>
+            <tr>
+                <th>Key</th>
+                <th>Value</th>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td><?php echo $dosen['nama']; ?></td>
+            </tr>
+            <tr>
+                <td>Domisili</td>
+                <td><?php echo $dosen['domisili']; ?></td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td><?php echo $dosen['jenis_kelamin']; ?></td>
+            </tr>
+        </table>
     </body>
 </html>
