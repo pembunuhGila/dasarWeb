ab88736e13879050ce1ca3d6f0548b74b88bd322
= <kiminsetyawan@gmail.com>
2025-09-30 17:28:15 +0700

    kurang echo

--- JS05_PHP-2/recursive.php
+++ JS05_PHP-2/recursive.php
@@ -1,6 +1,7 @@
 <?php
 
 function tampilkanAngka (int $jumlah, int $indeks = 1) {
+    echo "Perulangan ke-{$indeks} <br>";
     if($indeks < $jumlah) {
         tampilkanAngka($jumlah, $indeks + 1);
     }
