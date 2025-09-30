5c5ff8abc4f0443432479136af74e4228c768d0b
= <kiminsetyawan@gmail.com>
2025-09-30 16:59:39 +0700

    Soal no. 8

--- JS05_PHP-2/function_return.php
+++ JS05_PHP-2/function_return.php
@@ -4,5 +4,12 @@ function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
+function perkenalan($nama, $salam="Assalamualaikum") {
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br>";
 
-echo "Umur saya adalah " . hitungUmur(2006, 2025) . "tahun.";
+    echo "Saya berusia". hitungUmur(2006, 2025) . "tahun<br>";
+    echo "Senang berkenalan dengan Anda<br>";
+}
+perkenalan("Elok");
+?>
