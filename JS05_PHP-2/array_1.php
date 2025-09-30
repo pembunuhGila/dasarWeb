34131d9f022b71f26c5324bf58a1ac1d7469bdb4
= <kiminsetyawan@gmail.com>
2025-09-30 09:21:45 +0700

    Prak 1 langkah 1

---
+++ JS05_PHP-2/array_1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <h2>Array Terindeks</h2>
+        <?php
+        $listdosen=["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+
+        echo $listdosen[2] . "<br>";
+        echo $listdosen[0] . "<br>";
+        echo $listdosen[1] . "<br>";
+        ?>
+    </body>
+</html>
