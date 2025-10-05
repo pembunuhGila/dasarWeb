2116bb2a9a67424f0238decd19b069a977c713ed
= <kiminsetyawan@gmail.com>
2025-10-05 19:57:10 +0700

    Salah baca

--- JS05_PHP-2/date.php
+++ JS05_PHP-2/date.php
@@ -1,11 +1,14 @@
-<html>
+ <!DOCTYPE html>
+ <html>
     <head>
     </head>
     <body>
-        <h3>Time</h3>
+        <h3>Date</h3>
         <?php
-            date_default_timezone_set("asia/jakarta");
-            echo date("h:i:sa");
+            echo "Today is " . date("Y/m/d") . "<br>";
+            echo "Today is " . date("Y.m.d") . "<br>";
+            echo "Today is " . date("Y-m-d") . "<br>";
+            echo "Today is " . date("l");
         ?>
      </body>
 </html>
