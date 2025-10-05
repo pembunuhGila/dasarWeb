fd1e8f3a756a13bc8279ad5163395f4d1664b356
= <kiminsetyawan@gmail.com>
2025-10-05 19:00:15 +0700

    Soal no 5

--- praktik_php/fungsi.php
+++ praktik_php/fungsi.php
@@ -1,11 +1,16 @@
 <?php
 
-function perkenalan() {
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Elok<br/>";
-    echo "Senang berkenalan dengan Anda<br/>";
+function perkenalan($nama, $salam) {
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br>";
+    echo "Senang berkenalan dengan Anda<br>";
 }
+perkenalan("Hamdana", "Hallo");
 
-perkenalan();
-perkenalan();
+echo "<hr>";
+
+$saya = "Elok";
+$ucapanSalam = "Selamat pagi";
+
+perkenalan($saya, $ucapanSalam);
 ?>
