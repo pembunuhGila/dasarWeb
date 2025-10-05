3e11bc764617e599cfbef3b85bb41d1ac66b6e1a
= <kiminsetyawan@gmail.com>
2025-10-05 19:53:50 +0700

    Date and time Soal no 19

--- praktik_php/date.php
+++ praktik_php/date.php
@@ -1,8 +1,14 @@
-<?php
-echo '<html>';
-echo '<head><title>Cara02</title></head>';
-echo '<body>';
-echo '<p>Tanggal Hari ini : '.date('d M Y'). '</p>';
-echo '</body>';
-echo '</html>';
-?>
+ <!DOCTYPE html>
+ <html>
+    <head>
+    </head>
+    <body>
+        <h3>Date</h3>
+        <?php
+            echo "Today is " . date("Y/m/d") . "<br>";
+            echo "Today is " . date("Y.m.d") . "<br>";
+            echo "Today is " . date("Y-m-d") . "<br>";
+            echo "Today is " . date("l");
+        ?>
+     </body>
+</html>
