65adbdc0e18f19a0a4b274a1fcb87526d11499d9
= <kiminsetyawan@gmail.com>
2025-10-05 18:11:36 +0700

    Soal no 2

--- praktik_php/array_2.php
+++ praktik_php/array_2.php
@@ -4,18 +4,45 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title></title>
-        
+        <style>
+            table {
+                border-collapse: collapse;
+                width: 300px;
+            }
+            th, td {
+                border: 1px solid black;
+                padding: 8px;
+                text-align: left;
+            }
+            th {
+                background-color: #f2f2f2;
+            }
+        </style>
     </head>
     <body>
         <?php
-         $Dosen = [
+         $dosen = [
             'nama' => 'Elok Nur Hamdana',
             'domisili' => 'Malang',
             'jenis_kelamin' => 'Perempuan'];
-
-        echo "Nama : {$Dosen['nama']} <br>";
-        echo "Domisili : {$Dosen['domisili']} <br>";
-        echo "Jenis Kelamin : {$Dosen['jenis_kelamin']} <br>";
         ?>
+        <table>
+            <tr>
+                <th>Key</th>
+                <th>Value</th>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td><?php echo $dosen['nama']; ?></td>
+            </tr>
+            <tr>
+                <td>Domisili</td>
+                <td><?php echo $dosen['domisili']; ?></td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td><?php echo $dosen['jenis_kelamin']; ?></td>
+            </tr>
+        </table>
     </body>
 </html>
