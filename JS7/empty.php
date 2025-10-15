a04b1d01aafb2d5928656cd131b88cf8a9c5cbfd
= <kiminsetyawan@gmail.com>
2025-10-15 12:11:31 +0700

    langkah ke 4

--- JS7/empty.php
+++ JS7/empty.php
@@ -4,3 +4,9 @@ if (empty($myArray)) { <br>
 } else { <br>
     echo "Array terdefinisi dan tidak kosong."; <br>
 }
+
+if (empty($nonExistentVar)) {
+    echo "Variabel tidak terdefinisi atau kosong.";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong.";
+}
