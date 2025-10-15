7b7b4475ffaa8785bb5fe818532dfafc07ff69cb
= <kiminsetyawan@gmail.com>
2025-10-15 12:05:37 +0700

    soal no 2.1

--- JS7/empty.php
+++ JS7/empty.php
@@ -1,6 +1,6 @@
-$myArray = array();
-if (empty($myArray)) {
-    echo "Array tidak terdefinisi atau kosong.";
-} else {
-    echo "Array terdefinisi dan tidak kosong.";
+$myArray = array();<br>
+if (empty($myArray)) { <br>
+    echo "Array tidak terdefinisi atau kosong."; <br>
+} else { <br>
+    echo "Array terdefinisi dan tidak kosong."; <br>
 }
