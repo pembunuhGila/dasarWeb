41e9d7dfb9c4e4eef3d9b01f83fa04c425ab1023
= <kiminsetyawan@gmail.com>
2025-10-23 08:43:21 +0700

    soal 5.4

--- JS7/regex.php
+++ JS7/regex.php
@@ -21,7 +21,7 @@ $text = 'I like apple pie.';
 $new_text = preg_replace($pattern, $replacement, $text);
 echo $new_text . "<br>";  
 
-$pattern = '/go{1,3}d/';
+$pattern = '/go*d/';
 $text = 'god is good.';
 if (preg_match($pattern, $text, $matches)) {
     echo "Cocokkan: " . $matches[0];
