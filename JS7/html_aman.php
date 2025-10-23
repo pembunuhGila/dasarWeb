1c644b27e87850ffd44842ac5073fe385e3b49fd
= <kiminsetyawan@gmail.com>
2025-10-23 07:39:16 +0700

    4.1

--- JS7/html_aman.php
+++ JS7/html_aman.php
@@ -2,7 +2,7 @@
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-    <title>Contoh Input dan Output PHP</title>
+    <title>Contoh Input dan Output Aman</title>
 </head>
 <body>
     <form method="POST" action="">
@@ -13,13 +13,8 @@
 
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Ambil data dari form
-        $input = $_POST['input'];
-
-        // Amankan data dari karakter berbahaya
+        $input = $_POST['input'] ?? '';
         $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-
-        // Tampilkan hasil
         echo "<h3>Hasil Input:</h3>";
         echo "<p>$input</p>";
     }
