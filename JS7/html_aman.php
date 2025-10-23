f8a554af5c09a518628f3e690a692336b40352d2
= <kiminsetyawan@gmail.com>
2025-10-23 07:47:00 +0700

    4.2 final

--- JS7/html_aman.php
+++ JS7/html_aman.php
@@ -1,38 +1,37 @@
 <!DOCTYPE html>
-<html>
+<html lang="id">
 <head>
-    <title>Form Validasi Email</title>
+    <meta charset="UTF-8">
+    <title>Validasi Email Aman</title>
 </head>
 <body>
     <h2>Form Validasi Email</h2>
     <?php
-    $emailErr = "";
     $email = "";
-
+    $error = "";
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_POST['email'])) {
-            $email = $_POST['email'];
-        }
+        $email = $_POST['email'] ?? '';
         if (empty($email)) {
-            $emailErr = "Email harus diisi!";
-        } else {
-            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {  
-                $email = htmlspecialchars($email, ENT_QUOTES, 'UTF-8');
-                echo "<p style='color: green;'><strong>Data berhasil disimpan!</strong></p>";
-                echo "<p>Email yang tervalidasi dan diamankan: <strong>" . $email . "</strong></p>";
-            } else {
-                $emailErr = "Format email tidak valid!";
-                $email = htmlspecialchars($email, ENT_QUOTES, 'UTF-8');
-            }
+            $error = "Email wajib diisi!";
+        } 
+        elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $error = "Format email tidak valid!";
+        } 
+        else {
+            $email = htmlspecialchars($email, ENT_QUOTES, 'UTF-8');
+            echo "<p><strong>Data berhasil disimpan!</strong></p>";
+            echo "<p>Email tervalidasi dan aman: <b>$email</b></p>";
         }
     }
     ?>
-    
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-        <label for="email_field">Email:</label>
-        <input type="text" name="email" id="email_field" value="<?php echo $email; ?>">
-        <span class="error"><?php echo $emailErr; ?></span><br><br>
-        <input type="submit" name="submit" value="Submit">
+
+    <!-- Form Input -->
+    <form method="POST" action="">
+        <label for="email">Masukkan Email:</label>
+        <input type="text" id="email" name="email" value="<?php echo $email; ?>">
+        <span style="color:red;"><?php echo $error; ?></span><br><br>
+        <button type="submit">Kirim</button>
     </form>
+
 </body>
 </html>
