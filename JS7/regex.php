8c78249e56081943fb738abe47e1f9f18438e056
= <kiminsetyawan@gmail.com>
2025-10-23 08:21:28 +0700

    Soal 5.3

--- JS7/regex.php
+++ JS7/regex.php
@@ -21,11 +21,5 @@ $text = 'I like apple pie.';
 $new_text = preg_replace($pattern, $replacement, $text);
 echo $new_text . "<br>";  
 
-$pattern = '/go{1,3}d/';
-$text = 'god is good.';
-if (preg_match($pattern, $text, $matches)) {
-    echo "Cocokkan: " . $matches[0];
-} else {
-    echo "Tidak ada yang cocok!";
-}
+
 ?>
