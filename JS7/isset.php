ed396bc3cb3186596a939f6de3142ef63a4b67bc
= <kiminsetyawan@gmail.com>
2025-10-23 07:09:15 +0700

    perbaikan

--- JS7/isset.php
+++ JS7/isset.php
@@ -3,7 +3,7 @@ $umur;
 if (isset($umur) && $umur >= 18) {
    echo " Anda sudah dewasa.<br>"; 
 } else {
-   echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan. <br><br>";
+   echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.<br>";
 }
 $data = array("nama" => "Jane", "usia" => 25);
 if (isset($data["nama"])) {
