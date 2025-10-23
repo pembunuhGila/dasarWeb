0015fd45a3d0feb1e2097dce0397d576ce48c029
= <kiminsetyawan@gmail.com>
2025-10-23 21:30:50 +0700

    Soal no 9

--- JS8/proses_upload.php
+++ JS8/proses_upload.php
@@ -1,21 +1,35 @@
 <?php
-$targetDirectory = "documents/";
+// Lokasi penyimpanan file yang diunggah
+$targetDirectory = "documents/"; // Catatan: Akan lebih baik diganti "images/"
+$maxsize = 5 * 1024 * 1024;
+$allowedExtensions = array("jpg", "jpeg", "png", "gif");
+
+// Periksa apakah direktori penyimpanan ada, jika tidak maka buat
 if (!file_exists($targetDirectory)) {
     mkdir($targetDirectory, 0777, true);
 }
 
 if ($_FILES['files']['name'][0]) {
     $totalFiles = count($_FILES['files']['name']);
+
     for ($i = 0; $i < $totalFiles; $i++) {
         $fileName = $_FILES['files']['name'][$i];
         $targetFile = $targetDirectory . $fileName;
 
-        if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
-            echo "File $fileName berhasil diunggah.<br>";
+        $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+        if (in_array($fileType, $allowedExtensions) && $_FILES['files']['size'][$i] <= $maxsize) {
+     
+            if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
+                echo "File $fileName berhasil diunggah.<br>";
+            } else {
+                echo "Gagal mengunggah file $fileName: Gagal memindahkan.<br>";
+            }
         } else {
-            echo "Gagal mengunggah file $fileName.<br>";
+            echo "File $fileName GAGAL diunggah: Ekstensi tidak diizinkan atau ukuran > 5MB.<br>";
         }
     }
 } else {
     echo "Tidak ada file yang diunggah.";
 }
+?>
