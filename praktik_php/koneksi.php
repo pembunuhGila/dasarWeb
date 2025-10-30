eee1a17c2fec6be5b63bd85c15962b26f377f789
= <kiminsetyawan@gmail.com>
2025-10-30 09:02:30 +0700

    JS 9 Praktikum 1 Soal 2 Revisi

--- praktik_php/koneksi.php
+++ praktik_php/koneksi.php
@@ -1,12 +1,23 @@
 <?php
-$host = "localhost";
-$user = "root";    
-$pass = "";   
-$db = "prakwebdb";  
+$host = "localhost"; 
+$user = "root";       
+$pass = "";           
+$db   = "prakwebdb"; 
 
 $connect = mysqli_connect($host, $user, $pass, $db);
 
-if (!$connect) {
-    die("Koneksi ke database GAGAL: " . mysqli_connect_error());
+if (mysqli_connect_errno()) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
 } 
+
+$sql = "INSERT IGNORE INTO user (id, username, password) VALUES (1, 'admin', '123')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Data berhasil dimasukkan.";
+} else {
+    echo "Terjadi kesalahan: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
 ?>
