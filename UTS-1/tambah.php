a3ec28708b2874fea7e5f3df9530a409ab7a85d8
= <kiminsetyawan@gmail.com>
2025-11-13 17:56:06 +0700

    revisi

--- UTS-1/tambah.php
+++ UTS-1/tambah.php
@@ -1,10 +1,6 @@
 <?php
 // tambah.php
-session_start();
-if (!isset($_SESSION['user'])) {
-    header("Location: login.php");
-    exit;
-}
+include 'auth.php';
 include 'config.php';
 
 if (isset($_POST['submit'])) {
